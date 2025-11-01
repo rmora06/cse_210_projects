cf20567730a61449d71a0c0eb6c6b1ffa62fe9a1
rmora06 <robertjouelmora@gmail.com>
2025-11-01 15:36:43 -0400

    Exercise 2 Solution

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Enter your grade: ");
+        string value = Console.ReadLine();
+        int grade = int.Parse(value);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your Final Grade is '{letter}'");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! YOU PASSED");
+        }
+        else
+        {
+            Console.WriteLine("Sorry! You did not pass. Try your best next time");
+        }
     }
 }
