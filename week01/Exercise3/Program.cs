1e7fb187ecd219ee1e441a9eea9d45d686cf78d2
rmora06 <robertjouelmora@gmail.com>
2025-11-01 16:37:19 -0400

    EJ 3 solution

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+
+        int guess = 0;
+
+        while (guess != magic)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.Write("You guessed it!");
+
+            }
+        }
     }
 }
