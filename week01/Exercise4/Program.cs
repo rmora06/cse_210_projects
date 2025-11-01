fd323771f9d69c4aef7e2da3893affca15f90bbb
rmora06 <robertjouelmora@gmail.com>
2025-11-01 17:21:57 -0400

    Ej 4 solution

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+        int sum = 0;
+        int largest = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+                sum = sum + number;
+                if (number > largest)
+                {
+                    largest = number;
+                }
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine("The sorted list is:");
+        
+        foreach (int num in numbers)
+        {
+             Console.WriteLine(num);
+        }
     }
 }
