879ca8ea6be7331127a70b9cf85a73eceeef251d
rmora06 <robertjouelmora@gmail.com>
2025-11-01 14:32:01 -0400

    Solution

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string l_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {l_name}, {name} {l_name}");
     }
 }
