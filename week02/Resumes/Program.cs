f05b9b5b3caa550e577999b811175feaec51d17d
rmora06 <robertjouelmora@gmail.com>
2025-11-05 21:28:17 -0400

    resume activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._company = "Champro";
+        job1._startYear = 2025;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Electronic Technician";
+        job2._company = "Germoso";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Robert Mora";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
